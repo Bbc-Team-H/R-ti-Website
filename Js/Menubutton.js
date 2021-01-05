4fcb2301899b85ad2ec5b73feb433ae327b7f58d
Gintoro-Tatsumi <74654807+Gintoro-Tatsumi@users.noreply.github.com>
2021-01-05 11:34:03 +0100

    menu button funktioniert jetzt

--- Js/Menubutton.js
+++ Js/Menubutton.js
@@ -1,10 +1,6 @@
 function MenuOpen() {
-    var menu = document.getElementById("nav");
-    if (menu.style.display === "block") {
-      menu.style.display = "none";
-    } else {
-      menu.style.display = "block";
-    }
-  }
-   
-  document.getElementById("burgerbutton").addEventListener("click", MenuOpen)
+  var menu = document.getElementById("nav");
+  menu.classList.toggle("hidden");
+}
+
+document.getElementById("burgerbutton").addEventListener("click", MenuOpen)
